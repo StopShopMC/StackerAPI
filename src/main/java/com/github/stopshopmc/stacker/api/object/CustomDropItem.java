906e5a53f8f164c0a2d4303997713e1f85d8bc50
SirBlobman <blobmansrealm@gmail.com>
2021-02-13 11:52:40 -0500

    3.0.1: Add looting option to CustomDropItem

--- src/main/java/com/github/stopshopmc/stacker/api/object/CustomDropItem.java
+++ src/main/java/com/github/stopshopmc/stacker/api/object/CustomDropItem.java
@@ -9,11 +9,21 @@ import com.github.sirblobman.api.utility.Validate;
 public class CustomDropItem {
     private final int chance, maxChance;
     private final ItemStack item;
+    private boolean looting;
     public CustomDropItem(ItemStack item, int chance, int maxChance) {
         if(chance > maxChance) throw new IllegalArgumentException("chance cannot be greater than maxChance!");
         this.chance = chance;
         this.maxChance = maxChance;
         this.item = Validate.notNull(item, "item must not be null!").clone();
+        this.looting = false;
+    }
+
+    public void setLooting(boolean value) {
+        this.looting = value;
+    }
+
+    public boolean isLootingEnabled() {
+        return this.looting;
     }
     
     public boolean calculateChance() {
